55a2bf03b27aee2a09442a19b64f9043362b30e3
Mukhammed <massalimov.mukhammed@mail.ru>
2024-12-18 15:47:51 +0500

    header

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,8 @@
 import "../scss/style.scss";
+import "./dynamic_adapt";
+
+const iconMenu = document.querySelector(".icon-menu");
+
+iconMenu.addEventListener("click", () => {
+   document.documentElement.classList.toggle("menu-open");
+});
